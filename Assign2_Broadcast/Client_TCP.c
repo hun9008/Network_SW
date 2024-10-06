f0693df4b20f462cbd1ea94ea2c8c5a75f235cc9
jyHun <younghune135@gmail.com>
2024-10-06 16:14:52 +0900

    FEATE : TCP base code

---
+++ Assign2_Broadcast/Client_TCP.c
@@ -0,0 +1,177 @@
+#include <winsock.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 512
+
+void err_quit(char *msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage( 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER|
+        FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(-1);
+}
+
+void err_display(char *msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage( 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER|
+        FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    printf("[%s] %s", msg, (LPCTSTR)lpMsgBuf);
+    LocalFree(lpMsgBuf);
+}
+
+// 메시지 송신을 담당하는 함수
+DWORD WINAPI SendMessageThread(LPVOID arg)
+{
+    SOCKET sock = (SOCKET)arg;
+    char buf[BUFSIZE + 1];
+    char temp[BUFSIZE + 1];
+    char nickname[20];
+    int retval;
+
+    printf("\n[SET NICKNAME] : ");
+    
+    if (fgets(nickname, 20, stdin) == NULL)
+        return 0;
+
+    int nlen = strlen(nickname);
+    if (nickname[nlen - 1] == '\n')
+        nickname[nlen - 1] = '\0';
+
+    while (1) {
+        printf("\n[Input Message] ");
+
+        if (fgets(temp, BUFSIZE + 1, stdin) == NULL)
+            break;
+        
+        if (strcmp(temp, "0\n") == 0) {
+            // 클라이언트가 종료를 원할 때 '0'을 서버에 보내고 종료
+            send(sock, "0", 1, 0);
+            break;
+        }
+
+        sprintf(buf, "[%s] %s", nickname, temp);
+
+        int len = strlen(buf);
+        if (buf[len - 1] == '\n')
+            buf[len - 1] = '\0';
+
+        if (strlen(buf) == 0) {
+            printf("Message is empty\n");
+            continue;
+        }
+
+        // 서버로 메시지 전송
+        retval = send(sock, buf, strlen(buf), 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("send()");
+            continue;
+        }
+
+        printf("[TCP Client] Sent %d bytes.\n", retval);
+    }
+    
+    return 0;
+}
+
+// 메시지 수신을 담당하는 함수
+DWORD WINAPI ReceiveMessageThread(LPVOID arg)
+{
+    SOCKET sock = (SOCKET)arg;
+    char buf[BUFSIZE + 1];
+    int retval;
+
+    while (1) {
+        // 메시지 수신
+        retval = recv(sock, buf, BUFSIZE, 0);
+            
+        if (retval == SOCKET_ERROR) {
+            err_display("recv()");
+            continue;
+        } else if (retval == 0) {
+            // 서버에서 연결 종료
+            printf("Server closed the connection.\n");
+            break;
+        }
+
+        buf[retval] = '\0';  // 수신된 메시지 처리
+        printf("[TCP Client] Received %d bytes from server.\n", retval);
+        printf("[Received Message] %s\n", buf);
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    char ip[20] = "";
+    int port;
+
+    // 파라미터 확인
+    if (argc != 3) {
+        printf("Parameter Error\n");
+        return -1;
+    } else {
+        strcpy(ip, argv[1]);
+        port = atoi(argv[2]);
+        printf("IP : %s\n", ip);
+        printf("Port : %d\n", port);
+    }
+
+    int retval;
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return -1;
+
+    // TCP 소켓 생성
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) err_quit("socket()");
+
+    // 서버 주소 설정
+    SOCKADDR_IN serveraddr;
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(port);
+    serveraddr.sin_addr.s_addr = inet_addr(ip);
+
+    // 서버에 연결 요청
+    retval = connect(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("connect()");
+
+    printf("[TCP Client] Connected to the server.\n");
+
+    // 스레드 생성
+    HANDLE sendThread, recvThread;
+    DWORD sendThreadId, recvThreadId;
+
+    sendThread = CreateThread(NULL, 0, SendMessageThread, (LPVOID)sock, 0, &sendThreadId);
+    if (sendThread == NULL) err_quit("CreateThread() for send");
+
+    recvThread = CreateThread(NULL, 0, ReceiveMessageThread, (LPVOID)sock, 0, &recvThreadId);
+    if (recvThread == NULL) err_quit("CreateThread() for recv");
+
+    // 스레드 핸들 닫기
+    WaitForSingleObject(sendThread, INFINITE);
+    WaitForSingleObject(recvThread, INFINITE);
+
+    CloseHandle(sendThread);
+    CloseHandle(recvThread);
+
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
+}
---
+++ Assign2_Broadcast/thread_TCP_server.c
@@ -0,0 +1,281 @@
+#include <winsock.h>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <time.h>
+
+#define BUFSIZE 512
+#define MAX_CLIENTS 100
+
+int sum_messages = 0;
+int sum_bytes = 0;
+time_t start_time;
+time_t current_time;
+int running_time = 0;
+int server_running = 1; // 서버 실행 상태 플래그
+
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage( 
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(-1);
+}
+
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage( 
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (LPCTSTR)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+void printCommandMenu() {
+    printf("****************************************\n");
+    printf("*              COMMAND MENU            *\n");
+    printf("****************************************\n");
+    printf("*      ___                             *\n");
+    printf("*     |   |      Press 'i' to get      *\n");
+    printf("*     | i |   -> Client Info           *\n");
+    printf("*     |___|                            *\n");
+    printf("*                                      *\n");
+    printf("*      ___                             *\n");
+    printf("*     |   |      Press 's' to get      *\n");
+    printf("*     | s |   -> Chat Statistics       *\n");
+    printf("*     |___|                            *\n");
+    printf("*                                      *\n");
+    printf("*      ___                             *\n");
+    printf("*     |   |      Press 'q' to Quit     *\n");
+    printf("*     | q |                            *\n");
+    printf("*     |___|                            *\n");
+    printf("*                                      *\n");
+    printf("****************************************\n");
+}
+
+typedef struct {
+    SOCKET socket;  
+    SOCKADDR_IN addr;  
+    int active;      
+    char nickname[20]; 
+} ClientInfo;
+
+ClientInfo clients[MAX_CLIENTS];  // 클라이언트 정보 저장 배열
+
+void add_client(SOCKET client_sock, SOCKADDR_IN* clientaddr, char* nickname) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (!clients[i].active) {
+            clients[i].socket = client_sock;
+            clients[i].addr = *clientaddr;
+            clients[i].active = 1;
+            strcpy(clients[i].nickname, nickname);
+            printf("New client added: [%s] %s:%d\n", nickname, inet_ntoa(clientaddr->sin_addr), ntohs(clientaddr->sin_port));
+            return;
+        }
+    }
+    printf("Client list is full!\n");
+}
+
+void remove_client(SOCKET client_sock) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].socket == client_sock) {
+            clients[i].active = 0;
+            closesocket(clients[i].socket);
+            printf("Client disconnected: %s:%d\n", inet_ntoa(clients[i].addr.sin_addr), ntohs(clients[i].addr.sin_port));
+            return;
+        }
+    }
+}
+
+int is_known_client(SOCKET client_sock) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active && clients[i].socket == client_sock) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void broadcast_message(SOCKET sender_sock, char* buf, int len) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active && clients[i].socket != sender_sock) {
+            // 메시지를 보낸 클라이언트를 제외한 나머지에게 메시지를 전송
+            int retval = send(clients[i].socket, buf, len, 0);
+            if (retval == SOCKET_ERROR) {
+                err_display("send()");
+            }
+        }
+    }
+}
+
+DWORD WINAPI ProcessClient(LPVOID arg) {
+    SOCKET client_sock = (SOCKET)arg;
+    char buf[BUFSIZE + 1];
+    int retval;
+
+    while (server_running) {
+        retval = recv(client_sock, buf, BUFSIZE, 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("recv()");
+            break;
+        } else if (retval == 0) {
+            // 클라이언트가 연결을 끊음
+            remove_client(client_sock);
+            break;
+        }
+
+        buf[retval] = '\0';
+        printf("Message from client: %s\n", buf);
+        sum_messages++;
+        sum_bytes += retval;
+
+        // 에코 메시지: 보낸 클라이언트를 제외한 모든 클라이언트에게 전송
+        broadcast_message(client_sock, buf, retval);
+    }
+
+    return 0;
+}
+
+void printClientInfo() {
+    int active_clients_num = 0;
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active) {
+            active_clients_num++;
+        }
+    }
+
+    printf("****************************************\n");
+    printf("*              CLIENT INFO             *\n");
+    printf("****************************************\n");
+    printf("*           Client Number : %d          *\n", active_clients_num);
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active) {
+            printf("* %-20s\t%s:%d *\n", clients[i].nickname, inet_ntoa(clients[i].addr.sin_addr), ntohs(clients[i].addr.sin_port));
+        }
+    }
+    printf("****************************************\n");
+}
+
+void printChatStatistics() {
+    current_time = time(NULL);
+    running_time = (int)difftime(current_time, start_time);
+
+    printf("****************************************\n");
+    printf("*           CHAT STATISTICS            *\n");
+    printf("****************************************\n");
+    printf("* Msg/min : %-10d*\n", sum_messages / running_time * 60);
+    printf("* Bytes/min : %-8d*\n", sum_bytes / running_time * 60);
+    printf("* Total Msgs : %-7d*\n", sum_messages);
+    printf("* Total Bytes : %-6d*\n", sum_bytes);
+    printf("* Total Time : %-7d*\n", running_time);
+    printf("****************************************\n");
+}
+
+void printQuit() {
+    printf("****************************************\n");
+    printf("*              I'm Quit!               *\n");
+    printf("****************************************\n");
+    server_running = 0; // 서버 종료 플래그 설정
+}
+
+DWORD WINAPI ProcessStocastic(LPVOID arg) {
+    char command[2];
+
+    while (server_running) {
+        if (fgets(command, 2, stdin) == NULL)
+            return 0;
+
+        if (command[0] == 'i') {
+            printClientInfo();
+        } else if (command[0] == 's') {
+            printChatStatistics();
+        } else if (command[0] == 'q') {
+            printQuit();
+        } else {
+            printf("Invalid Command\n");
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    SOCKET listen_sock, client_sock;
+    SOCKADDR_IN serveraddr, clientaddr;
+    int addrlen;
+    HANDLE mThread, sThread;
+    DWORD mThreadId, sThreadId;
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return -1;
+
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(9000);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("bind()");
+
+    retval = listen(listen_sock, SOMAXCONN); // 클라이언트 연결 대기
+    if (retval == SOCKET_ERROR) err_quit("listen()");
+
+    start_time = time(NULL); // 서버 시작 시간 기록
+
+    printCommandMenu();
+    printf("[TCP SERVER READY] success bind & listen.\n");
+
+    // 서버 명령어 처리 스레드 생성
+    sThread = CreateThread(NULL, 0, ProcessStocastic, NULL, 0, &sThreadId);
+    if (sThread == NULL) err_quit("CreateThread()");
+
+    while (server_running) {
+        addrlen = sizeof(clientaddr);
+        client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen); // 클라이언트 접속 수락
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            continue;
+        }
+
+        // 클라이언트의 nickname을 수신하여 추가
+        char nickname[20];
+        retval = recv(client_sock, nickname, sizeof(nickname) - 1, 0);
+        if (retval <= 0) {
+            closesocket(client_sock);
+            continue;
+        }
+        nickname[retval] = '\0'; // 닉네임 문자열 종료
+
+        // 클라이언트 추가
+        add_client(client_sock, &clientaddr, nickname);
+
+        // 각 클라이언트 처리 스레드 생성
+        mThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)client_sock, 0, &mThreadId);
+        if (mThread == NULL) {
+            err_quit("CreateThread()");
+        }
+        CloseHandle(mThread); // 스레드 핸들 닫기
+    }
+
+    // 서버가 종료되면 소켓 정리
+    closesocket(listen_sock);
+    WSACleanup();
+    return 0;
+}
